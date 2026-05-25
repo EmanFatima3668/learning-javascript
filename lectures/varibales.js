152ee9bf824f5ae9e694b651c8890b2ddc52f659
Muhammad Ramzan Jamshaid <jamshaidali8u@gmail.com>
2026-05-25 06:49:51 +0000

    adding variable file

---
+++ lectures/varibales.js
@@ -0,0 +1,11 @@
+const accountId = 209990;
+
+let accountName = "Ramzan jamshaid";
+
+var accountEmail = "ramzanjamshaid8u@gmail.com"
+
+// some time javascrippt can us
+
+accountCity = "Farooqabad"
+
+console.table([accountId, accountName, accountEmail, accountCity]);
