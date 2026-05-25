f882e98d61fddae289388c5f32af50d27c5af92c
Muhammad Ramzan Jamshaid <jamshaidali8u@gmail.com>
2026-05-25 14:48:05 +0000

    adding new lecture 5

---
+++ lectures/datatype_comparison.js
@@ -0,0 +1,12 @@
+let score = 33;
+
+let stringNumber = String(score);
+
+console.log(typeof(stringNumber));
+console.log(stringNumber)
+
+// checking boolean to number 
+
+let ali = ""
+let ahmad = Boolean(ali)
+console.log(ahmad);
