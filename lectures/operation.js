447a26e03874cdd6e5a8a6b6081779fa34b8321c
Muhammad Ramzan Jamshaid <jamshaidali8u@gmail.com>
2026-05-25 15:31:50 +0000

    prefix and postfix file

---
+++ lectures/operation.js
@@ -0,0 +1,20 @@
+console.log( 2 + 2)
+console.log( 2 - 2)
+console.log( 2 / 2)
+console.log( 2 % 2)
+console.log( 2 * 2)
+console.log( 2 ** 2)
+console.log( 2 ** 3)
+// these are all simple type of opeation what if we use 
+
+console.log( "1" + 2)
+console.log( 1 + "2")
+console.log( 1 + "2" + 2)
+console.log( "1" + 2 + 2)
+console.log( 1 + 2 + "2")
+
+// 
+let x = 5
+ let y = x++ // is ma ya hota ha k x ki value 5 ha ab y ki phly jo k x sy assign hoi ha wo phly roint ho gi then us k bad
+ //  x ki value ma increement ho jaye ga 
+ console.log(x , y)
