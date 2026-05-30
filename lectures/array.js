fa251d2da121bafd24bbea9b9fb30afd07461ca0
Muhammad Ramzan Jamshaid <jamshaidali8u@gmail.com>
2026-05-30 05:53:53 +0000

    adding some more methods in array

--- lectures/array.js
+++ lectures/array.js
@@ -28,3 +28,19 @@ console.log(myArray)
 const Arr2 = myArray.splice(1,4)
 console.log(Arr1);
 console.log(myArray)
+
+
+
+let Arryback = [1 ,2 ,3 , [3, 5], 4, 5,6 , [3, 5, [ 6,4, [2, 4, 5, 6, ]]]]
+console.log(Arryback.flat(4))
+
+
+let finalArry = [...Arr1, ...Arr2, ...myArray]
+console.log(finalArry)
+
+
+let score = 2020202
+let secore1 = 3434433
+let secore2 = 494404040
+
+console.log(Array.of(score, secore1 , secore2))
