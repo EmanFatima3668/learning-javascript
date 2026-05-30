7741b036b3c94f46068985d9c939852eb6ebe245
Muhammad Ramzan Jamshaid <jamshaidali8u@gmail.com>
2026-05-30 04:26:59 +0000

    math and number file

---
+++ lectures/jsMath_and_numbers.js
@@ -0,0 +1,10 @@
+let number = 500;
+let othernumber = new Number (100);
+console.log(othernumber);
+console.log(othernumber.toString().length);
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10 + 1));
+
+
